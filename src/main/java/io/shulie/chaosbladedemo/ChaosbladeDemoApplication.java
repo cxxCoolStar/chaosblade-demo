14b6e2f659c600f44519ac55fb17c9f074ca1e4f
陈星星 <chenxingxing@shulie.io>
2022-10-28 18:00:05 +0800

    Initial commit

---
+++ src/main/java/io/shulie/chaosbladedemo/ChaosbladeDemoApplication.java
@@ -0,0 +1,13 @@
+package io.shulie.chaosbladedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosbladeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaosbladeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/shulie/chaosbladedemo/ChaosbladeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shulie.chaosbladedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosbladeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
